4d915f8129a1fc369641cd71180781731bfc1eb4
aravindh1211 <r.aravindh1211@gmail.com>
2025-08-24 21:49:37 +0530

    next.config.js

--- Frontend/next.config.js
+++ Frontend/next.config.js
@@ -7,14 +7,6 @@ const nextConfig = {
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000',
   },
-  async rewrites() {
-    return [
-      {
-        source: '/api/backend/:path*',
-        destination: `${process.env.BACKEND_URL}/:path*`,
-      },
-    ];
-  },
 }
 
 
